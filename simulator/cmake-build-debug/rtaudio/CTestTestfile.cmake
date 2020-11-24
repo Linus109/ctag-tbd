a0323aa11b4627950bfb5bd445ae7d7b1fd22290
Mathias <visuelle.musik@googlemail.com>
2020-11-24 21:35:38 +0100

    Deleted unwanted files
    For unknown reasons there were additional Files from the first Git clone...

--- simulator/cmake-build-debug/rtaudio/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/ctag-tbd/simulator/rtaudio
-# Build directory: C:/ctag-tbd/simulator/cmake-build-debug/rtaudio
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- simulator/cmake-build-debug/rtaudio/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/ctag-tbd/simulator/rtaudio/tests
-# Build directory: C:/ctag-tbd/simulator/cmake-build-debug/rtaudio/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(apinames "C:/ctag-tbd/simulator/cmake-build-debug/rtaudio/tests/apinames.exe")
-set_tests_properties(apinames PROPERTIES  _BACKTRACE_TRIPLES "C:/ctag-tbd/simulator/rtaudio/tests/CMakeLists.txt;32;add_test;C:/ctag-tbd/simulator/rtaudio/tests/CMakeLists.txt;0;")
